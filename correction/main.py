6bd4ab93a9e2c9721e1416347b595dca31d0f075
DummyBlue <86933320+DummyBlue@users.noreply.github.com>
2023-11-28 00:40:40 +0900

    오타 교정 모듈

---
+++ correction/main.py
@@ -0,0 +1,17 @@
+# This is Correction on Korean Typed words.
+# Please Refer https://github.com/HeegyuKim/symspellpy-ko
+
+from symspellpy_ko import KoSymSpell, Verbosity
+
+sym_spell = KoSymSpell()
+sym_spell.load_korean_dictionary(decompose_korean=True, load_bigrams=True)
+
+def lookup_words(term):
+    for suggestion in sym_spell.lookup_compound(term, max_edit_distance=2): # 편집거리 2 이하
+        print(f"{suggestion.term}")
+        print(f"{suggestion.distance}")
+        print(f"{suggestion.count}")
+
+if __name__ == '__main__':
+    tmp = input("Type words to correct: ").strip()
+    lookup_words(tmp)
