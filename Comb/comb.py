c8ce6a8918e674d798168d43b0de73b8fa40639b
lucete171 <80573129+lucete171@users.noreply.github.com>
2023-12-05 12:11:50 +0900

    Update comb.py

--- Comb/comb.py
+++ Comb/comb.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 data = {
     './testdata/turtle/0.wav': {'ㄱ': 80.0, 'ㄴ': 20.0, 'ㄷ': 0.0, 'ㅠ': 0.0, 'ㅊ': 0.0},
     './testdata/turtle/1.wav': {'ㅓ': 80.0, 'ㄴ': 20.0, 'ㅁ': 0.0, 'ㅠ': 0.0, 'ㅊ': 0.0},
@@ -8,20 +10,19 @@ data = {
     './testdata/turtle/6.wav': {'ㅣ': 100.0, 'ㅌ': 0.0, 'ㅈ': 0.0, 'ㅜ': 0.0, 'ㅕ': 0.0}
 }
 
-# 각 딕셔너리의 첫 번째 원소 추출
+# 첫 번째 원소만 추출하여 새로운 리스트 생성
 first_elements = [list(value.keys())[0] for value in data.values()]
 
-# 수치를 평균내기 위한 리스트 생성
-probabilities = []
+# 첫 번째 원소들의 확률 평균 계산
+total_probabilities = defaultdict(int)
+count = defaultdict(int)
 
-# 각 첫 번째 원소에 해당하는 수치 추출 및 평균 계산
-for key in first_elements:
-    # 해당 첫 번째 원소의 수치 추출하여 리스트에 추가
-    probs = [dic[key] for dic in data.values() if key in dic]
-    # 평균 계산하여 probabilities 리스트에 추가
-    average_prob = sum(probs) / len(probs)
-    probabilities.append(average_prob)
+for key, value in data.items():
+    first_key = list(value.keys())[0]
+    total_probabilities[first_key] += value[first_key]
+    count[first_key] += 1
 
-# 결과 출력
-result = dict(zip(first_elements, probabilities))
-print(result)
+averages = {key: total_probabilities[key] / count[key] for key in total_probabilities}
+
+print("첫 번째 원소:", first_elements)
+print("확률 평균:", averages)
