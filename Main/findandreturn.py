dea56921a2eecc0ed71a9482fd6c2f609ab51b38
DummyBlue <86933320+DummyBlue@users.noreply.github.com>
2023-12-07 14:27:47 +0900

    코드 최신화

--- Main/findandreturn.py
+++ Main/findandreturn.py
@@ -2,9 +2,8 @@ import librosa
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import f1_score
 
 def extract_mfcc(audio_path):
     y, sr = librosa.load(audio_path, sr=None)
@@ -25,7 +24,6 @@ def doProcess():
 
     cName = [path.split('_')[1] for path in cFolder]
     vName = [path.split('_')[1] for path in vFolder]
-
     for k in range(len(cFolder)):
         cFolder[k] = cPath + cFolder[k]
 
@@ -57,12 +55,6 @@ def doProcess():
     features = []
     labels = []
 
-    features_c = []
-    labels_c = []
-
-    features_v = []
-    labels_v = []
-
     # 자음/모음만 구분하기 위한 특성 추출
     for k in cFiles:
         for z in cFiles.get(k):
@@ -78,16 +70,12 @@ def doProcess():
     for k in cFiles:
         for z in cFiles.get(k):
             features.append(extract_mfcc(z))
-            features_c.append(extract_mfcc(z))
             labels.append(k)
-            labels_c.append(k)
 
     for k in vFiles:
         for z in vFiles.get(k):
             features.append(extract_mfcc(z))
-            features_v.append(extract_mfcc(z))
             labels.append(k)
-            labels_v.append(k)
 
     # 학습 비율 일괄 지정을 위한 변수
     ts_size = 0.3
@@ -103,31 +91,7 @@ def doProcess():
     y_pred = rf_modelcv.predict(X_test)
     print("자음과 모음만을 구분하는 모델:", f1_score(y_test, y_pred, average="micro"))
     # ----------
-    '''
-    # 자음만 학습한 모델
-    Xc = np.array(features_c)
-    yc = np.array(labels_c)
 
-    X_train, X_test, y_train, y_test = train_test_split(Xc, yc, test_size=ts_size)
-
-    rf_modelc = RandomForestClassifier()
-    rf_modelc.fit(X_train, y_train)
-    y_pred = rf_modelc.predict(X_test)
-    print("자음만을 구분하는 모델:", f1_score(y_test, y_pred, average="micro"))
-    # ----------
-
-    # 모음만 학습한 모델
-    Xv = np.array(features_v)
-    yv = np.array(labels_v)
-
-    X_train, X_test, y_train, y_test = train_test_split(Xv, yv, test_size=ts_size)
-
-    rf_modelv = RandomForestClassifier()
-    rf_modelv.fit(X_train, y_train)
-    y_pred = rf_modelv.predict(X_test)
-    print("모음만을 구분하는 모델:", f1_score(y_test, y_pred, average="micro"))
-    # ----------
-    '''
     # 모든 키를 학습하는 모델
     X = np.array(features)
     y = np.array(labels)
@@ -163,35 +127,11 @@ def doProcess():
             else:
                 tmp_vow = probability * 100
 
-        '''
-        if(tmp_con > tmp_vow): # 자음일 경우
-            c_prob = predict_new_audio(rf_modelc, audio_path)
-            print(f"Consonant Probabilities for {audio_path}:")
-            for labelc, probc in zip(rf_modelc.classes_, c_prob):
-                tmp_dict[labelc] = probc * 100
-                print(f"  {labelc} = {probc * 100:.2f}%")
-
-        elif(tmp_con < tmp_vow): # 모음일 경우
-            v_prob = predict_new_audio(rf_modelv, audio_path)
-            print(f"Vowel Probabilities for {audio_path}:")
-            for labelv, probv in zip(rf_modelv.classes_, v_prob):
-                tmp_dict[labelv] = probv * 100
-                print(f"  {labelv} = {probv * 100:.2f}%")
-
-        else: # 같은 경우
-            prob = predict_new_audio(rf_model, audio_path)
-            print(f"All Keys Probabilities for {audio_path}:")
-            for labelcv, probcv in zip(rf_model.classes_, prob):
-                tmp_dict[labelcv] = probcv * 100
-                print(f"  {labelcv} = {probcv * 100:.2f}%")
-        '''
-
         prob = predict_new_audio(rf_model, audio_path)
         print(f"All Keys Probabilities for {audio_path}:")
         for labelcv, probcv in zip(rf_model.classes_, prob):
             tmp_dict[labelcv] = probcv * 100
             print(f"  {labelcv} = {probcv * 100:.2f}%")
-
 
         print("\n")
 
--- Main/main.py
+++ Main/main.py
@@ -1,5 +1,4 @@
 from inspector import *
-from slicer import *
 from correction import *
 from comb import *
 from findandreturn import *
@@ -18,21 +17,21 @@ def rm_space(list):
     return tmp_word
 
 if __name__ == '__main__':
-    final_rst = doProcess()
+    final_rst = doProcess() # 음성을 글자로 변환
     print(final_rst)
 
-    mid_list = do_comb(final_rst)
+    mid_list = do_comb(final_rst) # 성학님 모듈을 이용해 합치기 수행
 
-    mid2_list = do_conv(mid_list)
+    mid2_list = do_conv(mid_list) # 영어를 한글로 변환
     print(mid2_list)
 
-    mid3_list = do_inspect(mid2_list)
+    mid3_list = do_inspect(mid2_list) # 글자들 합치기
     print(mid3_list)
 
-    mid4_list = rm_space(mid3_list)
+    mid4_list = rm_space(mid3_list) # 합치는 과정에서의 공백 제거
     print(mid4_list)
 
-    final_list = do_correction(mid4_list)
+    final_list = do_correction(mid4_list) # 교정 수행
     print(f"최종 결과: {final_list}")
 
 #main(data_1)
